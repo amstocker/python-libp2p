09e5605b28782d3b8f3b938b5ca05785d16efe1a
Andrew Stocker <amstocker@dons.usfca.edu>
2015-11-03 16:59:46 -0800

    test dir

---
+++ test/test_multiaddr.py
@@ -0,0 +1,27 @@
+from multiaddr import MultiAddress
+from multiaddr.utils.base58 import b58encode, b58decode
+
+with open('base58_encode_decode.json') as f:
+    import json
+    tests = json.loads(f.read())
+
+for d, e in tests:
+    if len(d) == 0:
+        continue
+    i = int(d, 16)
+    print("{} -> {} (should be: {})".format(d, b58encode(i), e))
+
+
+ma = MultiAddress("/ip4/127.0.0.1/tcp/4001/ipfs/QmerTTan8gkTEugcb4DmFpAw8Z7bkDQfhoGh6AHzQaqD1Y")
+
+print ma
+print len(ma.str_repr)
+print ma.as_bytes().__repr__()
+print len(ma.as_bytes())
+
+ma2 = MultiAddress(ma.as_bytes())
+
+print ma2
+print len(ma2.str_repr)
+print ma2.as_bytes().__repr__()
+print len(ma2.as_bytes())
