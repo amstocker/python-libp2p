1336e9a9ff301518ad4d505e2b0bd84a980ec0d8
Andrew Stocker <amstocker@dons.usfca.edu>
2015-11-04 00:01:57 -0800

    more reorg

---
+++ libp2p/protocol/multistream/__init__.py
@@ -0,0 +1,3 @@
+# Identification string of this protocol.  Note that on the wire this must be
+# encoded in UTF-8.
+PATH = "/multistream/1.0.0"
---
+++ libp2p/protocol/multistream/base_multiplexer.py
@@ -0,0 +1,5 @@
+"""
+Abstract base class for a protocol multiplexer.
+"""
+
+
